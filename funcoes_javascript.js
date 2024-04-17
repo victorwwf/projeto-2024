21449bec04139078900ecfe12d84a9c9a2976007
victorwwf <126262798+victorwwf@users.noreply.github.com>
2024-04-17 13:58:09 -0300

    Update funcoes_javascript.js

--- funcoes_javascript.js
+++ funcoes_javascript.js
@@ -1,3 +1,12 @@
 function somar(num1, num2){
   return num1+num2;
 }
+function subtração(num1, num2){
+  return num-num2;
+}
+function multiplicação(num1, num2){
+  return num1*num2;
+}
+function somar(num1, num2){
+  return num1*num2;
+}
